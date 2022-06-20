ab54bc00339ee9c10efc45b18dd7b568742e34c9
nadzlaandrita <nadzlaandrita18@gmail.com>
2022-06-20 14:33:56 +0700

    ini tambahan gue

--- src/userdata/Customer.java
+++ src/userdata/Customer.java
@@ -1,9 +1,23 @@
 package userdata;
 
-public class Customer extends User{
+import java.util.Random;
 
+public class Customer extends User{
+	Random rand = new Random();
+	
 	public Customer() {
-		
+		super();
+	}
+	
+	public Customer(String id, String email, String password, String name, String type) {
+		super(id, email, password, name, type);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String generateId() {
+
+		return "CU" + rand.nextInt(10) + rand.nextInt(10) + rand.nextInt(10);
 	}
 
 }
