e420c922fb0dba18164ca6a6f1cdb3c75175a7d3
DarkMattz <febryangrady@gmail.com>
2022-06-21 22:40:20 +0700

    Update DB Handler

---
+++ src/userdata/Admin.java
@@ -0,0 +1,11 @@
+package userdata;
+
+
+public class Admin extends User{
+		
+	public Admin(String email, String password) {
+		super(email, password);
+		
+	}
+
+}
---
+++ src/userdata/Customer.java
@@ -0,0 +1,32 @@
+package userdata;
+
+
+public class Customer extends User{
+	private String name;
+	private String phone;
+	
+	public Customer(String email, String password, String name, String phone) {
+		super(email, password);
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
