49f6e40fb8a5878b2ebaf232465664d0e877b797
DarkMattz <febryangrady@gmail.com>
2022-07-01 21:32:20 +0700

    Add Binary for DB

--- src/database/DatabaseUserHandler.java
+++ src/database/DatabaseUserHandler.java
@@ -11,7 +11,7 @@ public class DatabaseUserHandler {
     private Connect con = Connect.getConnection();
 
     public boolean checkEmail(String email) {
-        ResultSet res = con.executeQuery("SELECT * FROM users WHERE email = '" + email + "'");
+        ResultSet res = con.executeQuery("SELECT * FROM users WHERE BINARY email = '" + email + "'");
         try {
             return res.next();
         } catch (SQLException e) {
@@ -42,7 +42,7 @@ public class DatabaseUserHandler {
     }
 
     public String getName(String email) {
-        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE email = '" + email + "'");
+        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE BINARY email = '" + email + "'");
         try {
             if(res.next()){
                 return res.getString("name");
@@ -54,7 +54,7 @@ public class DatabaseUserHandler {
     }
 
     public Customer findCustomer(String email, String password) {
-        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE email = '" + email + "' AND password = '" + password +"'");
+        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE BINARY email = '" + email + "' AND password = '" + password +"'");
         try {
             if(res.next()){
                 return new Customer(res.getString("email"), res.getString("password"), res.getString("name"), res.getString("phone"));
@@ -66,7 +66,7 @@ public class DatabaseUserHandler {
     }
 
     public Admin findAdmin(String email, String password) {
-        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE email = '" + email + "' AND password = '" + password +"' AND isAdmin = " + "1");
+        ResultSet res = con.executeQuery("SELECT * FROM `users` WHERE BINARY email = '" + email + "' AND password = '" + password +"' AND isAdmin = " + "1");
         try {
             if(res.next()){
                 return new Admin(res.getString("email"), res.getString("password"));
@@ -78,15 +78,15 @@ public class DatabaseUserHandler {
     }
 
     public void editName(Customer customer, String name) {
-        con.executeUpdate("UPDATE `users` SET `name`='"+ name +"' WHERE `email` = '" + customer.getEmail() + "'");
+        con.executeUpdate("UPDATE `users` SET `name`='"+ name +"' WHERE BINARY `email` = '" + customer.getEmail() + "'");
     }
 
     public void editPhone(Customer customer, String phone) {
-        con.executeUpdate("UPDATE `users` SET `phone`='"+ phone +"' WHERE `email` = '" + customer.getEmail() + "'");
+        con.executeUpdate("UPDATE `users` SET `phone`='"+ phone +"' WHERE BINARY `email` = '" + customer.getEmail() + "'");
     }
 
     public void editPass(Customer customer, String pass) {
-        con.executeUpdate("UPDATE `users` SET `password`='"+ pass +"' WHERE `email` = '" + customer.getEmail() + "'");
+        con.executeUpdate("UPDATE `users` SET `password`='"+ pass +"' WHERE BINARY `email` = '" + customer.getEmail() + "'");
     }
 
 }
